84bc587f549a00c40549b959fb4b3ea3537ada0b
Qcbit <5490853+qcbit@users.noreply.github.com>
2023-11-16 07:15:44 -0600

    Logging

---
+++ app/services/node/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/qcbit/blockchain/foundation/logger"
+	"go.uber.org/zap"
+)
+
+func main() {
+	log, err := logger.New("QChain")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	if err := run(log); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+
+	// ----------------------------------------------------------------
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	// ----------------------------------------------------------------
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+	log.Infow("shutdown", "status", "shutdown signal")
+	defer log.Infow("shutdown", "status", "complete", "signal")
+
+	return nil
+}
