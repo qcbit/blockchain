35abc39e2babc49ceb20ad978033f3a605a2048b
Qcbit <5490853+qcbit@users.noreply.github.com>
2023-11-28 19:58:18 -0600

    scratch

--- app/scratch/main.go
+++ app/scratch/main.go
@@ -4,35 +4,74 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
-	"crypto/sha256"
-	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 type Tx struct {
 	FromID string `json:"from_id"`
 	ToID   string `json:"to_id"`
-	Value uint64 `json:"value"`
+	Value  uint64 `json:"value"`
 }
 
 func main() {
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		panic(err)
+	if err := run(); err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
 	}
-
-	msg := []byte("hello, world")
-	hash := sha256.Sum256(msg)
-
-	sig, err := ecdsa.SignASN1(rand.Reader, privateKey, hash[:])
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Signature:", base64.StdEncoding.EncodeToString(sig))
-
-	valid := ecdsa.VerifyASN1(&privateKey.PublicKey, hash[:], sig)
-	publicKey := privateKey.PublicKey
-	fmt.Println("Public key:", base64.StdEncoding.EncodeToString(elliptic.Marshal(publicKey.Curve, publicKey.X, publicKey.Y)))
-	fmt.Println("Valid signature:", valid)
 }
 
+func run() error {
+	// privateKey, err := GenerateKey()
+	privateKey, err := crypto.HexToECDSA("9f332e3700d8fc2446eaf6d15034cf96e0c2745e40353deef032a5dbf1dfed93")
+	if err != nil {
+		return fmt.Errorf("failed to generate key: %w", err)
+	}
+
+	tx := Tx{
+		FromID: "alice",
+		ToID:   "bob",
+		Value:  100,
+	}
+
+	data, err := json.Marshal(tx)
+	if err != nil {
+		return fmt.Errorf("failed to marshal tx: %w", err)
+	}
+
+	stamp := []byte(fmt.Sprintf("\x19Q Signed Message:\n%d", len(data)))
+
+	v := crypto.Keccak256(stamp, data)
+
+	// 32-byte hashed data
+	sig, err := crypto.Sign(v, privateKey)
+	if err != nil {
+		return fmt.Errorf("failed to sign tx: %w", err)
+	}
+
+	fmt.Println("Signature:", hex.EncodeToString(sig))
+
+	publicKey, err := crypto.SigToPub(v, sig)
+	if err != nil {
+		return fmt.Errorf("failed to get public key: %w", err)
+	}
+
+	fmt.Println("Public address:", crypto.PubkeyToAddress(*publicKey).Hex())
+
+	return nil
+}
+
+func GenerateKey() (*ecdsa.PrivateKey, error) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+
+	return privateKey, nil
+}
