48a8e818b90c5591d106b96c025c2d1c2c6d129a
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-01-16 05:16:28 -0600

    initial commit

---
+++ app/wallet/cli/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/qcbit/blockchain/app/wallet/cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ foundation/blockchain/genesis/genesis.go
@@ -0,0 +1,36 @@
+// Package genesis maintains access to the genesis file.
+package genesis
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
+
+// Genesis is the genesis file.
+type Genesis struct {
+	Date          time.Time         `json:"date"`
+	ChainID       uint16            `json:"chain_id"`
+	TransPerBlock uint16            `json:"trans_per_block"`
+	Difficulty    uint16            `json:"difficulty"`
+	MinerReward   uint64            `json:"miner_reward"`
+	GasPrice      uint64            `json:"gas_price"`
+	Balance       map[string]uint64 `json:"balances"`
+}
+
+// Load loads the genesis file.
+func Load() (Genesis, error) {
+	path := "zblock/genesis.json"
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return Genesis{}, err
+	}
+
+	var genesis Genesis
+	err = json.Unmarshal(content, &genesis)
+	if err != nil {
+		return Genesis{}, err
+	}
+
+	return genesis, nil
+}
