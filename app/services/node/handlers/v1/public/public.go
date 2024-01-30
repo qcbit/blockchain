41204e2f816c8bb0d470aff21f0f039eb3501de3
Qcbit <5490853+qcbit@users.noreply.github.com>
2024-01-29 20:43:15 -0600

    submit wallet transaction node support

--- app/services/node/handlers/v1/public/public.go
+++ app/services/node/handlers/v1/public/public.go
@@ -3,10 +3,12 @@ package public
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"go.uber.org/zap"
 
+	v1 "github.com/qcbit/blockchain/business/web/v1"
 	"github.com/qcbit/blockchain/foundation/blockchain/database"
 	"github.com/qcbit/blockchain/foundation/blockchain/state"
 	"github.com/qcbit/blockchain/foundation/web"
@@ -16,6 +18,39 @@ import (
 type Handlers struct {
 	Log   *zap.SugaredLogger
 	State *state.State
+}
+
+// SubmitWalletTransaction adds new transactions to the mempool.
+func (h Handlers) SubmitWalletTransaction(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	v, err := web.GetValues(ctx)
+	if err != nil {
+		return web.NewShutdownError("web value missing from context")
+	}
+
+	// Decode the JSON in the post call into a Signed transaction.
+	var signedTx database.SignedTx
+	if err := web.Decode(r, &signedTx); err != nil {
+		return fmt.Errorf("unable to decode payload: %w", err)
+	}
+
+	h.Log.Infow("add tran", "traceid", v.TraceID, "sig:nonce", signedTx, "from", signedTx.FromID,
+		"to", signedTx.ToID, "value", signedTx.Value, "tip", signedTx.Tip)
+
+	// Ask the state package to add this transaction to the mempool. Only the
+	// checks are the transaction signature and the recipient account format.
+	// It's up to the wallet to make sure the account has a proper blance and
+	// nonce. Fees will be taken if this transaction is mined into a block.
+	if err := h.State.UpsertWalletTransaction(signedTx); err != nil {
+		return v1.NewRequestError(err, http.StatusBadRequest)
+	}
+
+	resp := struct {
+		Status string `json:"status"`
+	}{
+		Status: "transaction added to mempool",
+	}
+
+	return web.Respond(ctx, w, resp, http.StatusOK)
 }
 
 // Genesis returns the genesis information.
